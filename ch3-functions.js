c326be391577f95d97c24b1a9a7260b1c040c8c2
Jung Lee <junglee43@gmail.com>
2018-12-04 22:11:59 -0500

    Recursive isEven.

--- ch3-functions.js
+++ ch3-functions.js
@@ -47,3 +47,14 @@ function isEven(num) {
         return isEven(num - 2);
     }
 }
+
+function isEven(num) {
+    if(num == 0) {
+        return true;
+    } else if(num == 1 || num < 0) {
+        return false;
+    } else {
+        //        num -= 2;
+        return isEven(num - 2);
+    }
+}
