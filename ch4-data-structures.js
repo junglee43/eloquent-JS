30194621596fd33a94aa65af0cfdcf623b6243ba
Jung Lee <junglee43@gmail.com>
2018-12-07 21:22:13 -0500

    Working on it.

--- ch4-data-structures.js
+++ ch4-data-structures.js
@@ -315,3 +315,27 @@ function reverseArrayInPlace(array) {
    Write a function arrayToList that builds up a list structure like the one shown when given [1, 2, 3] as argument. Also write a listToArray function that produces an array from a list. Then add a helper function prepend, which takes an element and a list and creates a new list that adds the element to the front of the input list, and nth, which takes a list and a number and returns the element at the given position in the list (with zero referring to the first element) or undefined when there is no such element.
    If you haven’t already, also write a recursive version of nth.
 */
+
+function arrayToList(array) {
+    let list = {value: '',
+                rest: ''};
+    for(let element of array) {
+        list.value = array.shift();
+        list.rest = arrayToList(array);
+    }
+    return list;
+}
+
+function arrayToList(array) {
+    let list = {value: '',
+                rest: ''};
+    for(let i = 0; i < array.length; i++) {
+        list.value = array.shift();
+        if(i == array.length) {
+            list.rest = null;
+        } else {
+            list.rest = arrayToList(array);
+        }
+    }
+    return list;
+}
